48dd3ebf40ba9397586d8315ba1a5ae8929b7b3e
henokworkuliyew <hw93gojj@gmail.com>
2025-09-06 22:43:41 +0300

    chore: export all auth-related forms from a single entry point

---
+++ frontend/src/components/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as LoginForm } from './LoginForm';
+export { default as RegisterForm } from './RegisterForm';
+export { default as ForgotPasswordForm } from './ForgotPasswordForm';
+export { default as OtpVerificationForm } from './OtpVerificationForm';
+export { default as ResetPasswordForm } from './ResetPasswordForm';
