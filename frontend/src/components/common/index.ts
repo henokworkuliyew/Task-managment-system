717833870f79be37d6e158023f81ae0958f9484c
henokworkuliyew <hw93gojj@gmail.com>
2025-09-06 22:50:35 +0300

    Export core UI components for centralized imports

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,4 @@
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Modal } from './Modal';
