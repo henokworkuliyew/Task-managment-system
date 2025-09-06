553559eefe36aefd8b6f632df736f2be7d97b4eb
henokworkuliyew <hw93gojj@gmail.com>
2025-09-06 23:02:48 +0300

    chore: export store, hooks, and all slices for centralized imports

---
+++ frontend/src/redux/index.ts
@@ -0,0 +1,7 @@
+export * from './store';
+export * from './hooks';
+export * from './slices/authSlice';
+export * from './slices/projectSlice';
+export * from './slices/taskSlice';
+export * from './slices/issueSlice';
+export * from './slices/notificationSlice';
