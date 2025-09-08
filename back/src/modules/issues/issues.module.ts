80965028f0c6ae5def8990a4c86578d2b6a6bece
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 00:10:45 +0300

    update issue module

--- back/src/modules/issues/issues.module.ts
+++ back/src/modules/issues/issues.module.ts
@@ -1,13 +1,18 @@
-import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { IssuesController } from "./issues.controller"
-import { IssuesService } from "./issues.service"
-import { Issue } from "../../entities/issue.entity"
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { IssuesService } from './issues.service'
+import { IssuesController } from './issues.controller'
+import { Issue } from '../../entities/issue.entity'
+import { User } from '../../entities/user.entity'
+import { Project } from '../../entities/project.entity'
+import { NotificationLog } from '../../entities/notification-log.entity'
+import { EmailService } from '../auth/email.service'
+import { NotificationsService } from '../notifications/notifications.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Issue])],
+  imports: [TypeOrmModule.forFeature([Issue, User, Project, NotificationLog])],
   controllers: [IssuesController],
-  providers: [IssuesService],
+  providers: [IssuesService, EmailService, NotificationsService],
   exports: [IssuesService],
 })
 export class IssuesModule {}
