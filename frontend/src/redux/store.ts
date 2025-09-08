dd64ef33c6d91d168b4220bebd8708b2a449eba7
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 01:17:02 +0300

    chore: configure Redux store with reducers and middleware

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureAppStore } from './storeConfig';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import projectReducer from './slices/projectSlice';
 import taskReducer from './slices/taskSlice';
@@ -6,13 +6,24 @@ import issueReducer from './slices/issueSlice';
 import notificationReducer from './slices/notificationSlice';
 
 // Configure the store with reducers
-export const store = configureAppStore({
-  auth: authReducer,
-  projects: projectReducer,
-  tasks: taskReducer,
-  issues: issueReducer,
-  notifications: notificationReducer,
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    projects: projectReducer,
+    tasks: taskReducer,
+    issues: issueReducer,
+    notifications: notificationReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+// Make store available globally for interceptors
+if (typeof window !== 'undefined') {
+  (window as typeof window & { __REDUX_STORE__: typeof store }).__REDUX_STORE__ = store;
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
