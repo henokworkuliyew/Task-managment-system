393e721267082e9abdd8ca5454dccc59ce7f8f89
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 00:05:31 +0300

    Add pending registration

--- back/src/modules/auth/auth.module.ts
+++ back/src/modules/auth/auth.module.ts
@@ -9,24 +9,24 @@ import { AuthController } from "./auth.controller"
 import { AuthService } from "./auth.service"
 import { JwtStrategy } from "./strategies/jwt.strategy"
 import { User } from "../../entities/user.entity"
+import { PendingRegistration } from "../../entities/pending-registration.entity"
 import { UsersModule } from "../users/users.module"
 import { EmailService } from "./email.service"
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, PendingRegistration]),
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>("JWT_SECRET"),
         signOptions: {
-          expiresIn: configService.get<string>("JWT_EXPIRES_IN") || "15m",
+          expiresIn: '24h',
         },
       }),
       inject: [ConfigService],
     }),
-    // Conditionally register BullModule queue only when Redis is available
     ConfigModule,
     UsersModule,
   ],
@@ -36,6 +36,6 @@ import { EmailService } from "./email.service"
     JwtStrategy,
     EmailService
   ],
-  exports: [AuthService],
+  exports: [AuthService, EmailService],
 })
 export class AuthModule {}
