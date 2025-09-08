988ad3952e6f517aba721de54116cfe0f9d29654
henokworkuliyew <hw93gojj@gmail.com>
2025-09-08 23:57:44 +0300

    TypeScript compilation errors and type safety improvements

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -14,6 +14,7 @@ import { NotificationsModule } from "./modules/notifications/notifications.modul
 import { TimeTrackingModule } from "./modules/time-tracking/time-tracking.module"
 import { ReportsModule } from "./modules/reports/reports.module"
 import { WebhooksModule } from "./modules/webhooks/webhooks.module"
+import { InvitationsModule } from "./modules/invitations/invitations.module"
 
 
 import { databaseConfig } from "./config/database.config"
@@ -76,6 +77,7 @@ import { cloudinaryConfig } from "./config/cloudinary.config"
     TimeTrackingModule,
     ReportsModule,
     WebhooksModule,
+    InvitationsModule,
   ],
   providers: [
     {
