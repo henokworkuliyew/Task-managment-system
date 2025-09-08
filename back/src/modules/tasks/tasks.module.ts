e52d80d88e83a5ffb1a9d77fc307ea8b89a97eba
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 00:16:26 +0300

    refactor(projects): clean up DTO with basic validation decorators

--- back/src/modules/tasks/tasks.module.ts
+++ back/src/modules/tasks/tasks.module.ts
@@ -1,13 +1,18 @@
-import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { TasksController } from "./tasks.controller"
-import { TasksService } from "./tasks.service"
-import { Task } from "../../entities/task.entity"
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TasksService } from './tasks.service'
+import { TasksController } from './tasks.controller'
+import { Task } from '../../entities/task.entity'
+import { User } from '../../entities/user.entity'
+import { Project } from '../../entities/project.entity'
+import { NotificationLog } from '../../entities/notification-log.entity'
+import { EmailService } from '../auth/email.service'
+import { NotificationsService } from '../notifications/notifications.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task, User, Project, NotificationLog])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, EmailService, NotificationsService],
   exports: [TasksService],
 })
 export class TasksModule {}
