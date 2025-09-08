5e7a32de146c71ff2d00e7355dc18ba05b892140
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 01:17:14 +0300

    feat: create configurable Redux store with dynamic reducers

--- frontend/src/redux/storeConfig.ts
+++ frontend/src/redux/storeConfig.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 
 // Create a function to configure the store with reducers
-export function configureAppStore(reducers: any) {
-  return configureStore({
-    reducer: reducers,
+export function configureAppStore(reducers: Record<string, unknown>) {
+  const rootReducer = combineReducers(reducers);
+
+  const store = configureStore({
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         serializableCheck: false,
       }),
   });
+  
+  return store;
 }
