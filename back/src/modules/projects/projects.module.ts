765a3c4ea5dbfd1d498d5add8f070b1f1e0e80e5
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 00:16:00 +0300

    feat(projects): register additional entities and services for invitations and notifications

--- back/src/modules/projects/projects.module.ts
+++ back/src/modules/projects/projects.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { ProjectsController } from "./projects.controller"
 import { ProjectsService } from "./projects.service"
 import { Project } from "../../entities/project.entity"
+import { User } from "../../entities/user.entity"
+import { ProjectInvitation } from "../../entities/project-invitation.entity"
+import { NotificationLog } from "../../entities/notification-log.entity"
+import { EmailService } from "../auth/email.service"
+import { NotificationsService } from "../notifications/notifications.service"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project, User, ProjectInvitation, NotificationLog])],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [ProjectsService, EmailService, NotificationsService],
   exports: [ProjectsService],
 })
 export class ProjectsModule {}
