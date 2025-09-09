d933b127c1bd6253ba2681e79bf8291e44f390cf
henokworkuliyew <hw93gojj@gmail.com>
2025-09-09 12:17:27 +0300

    fix deploymrnt error

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { setupInterceptors } from '../utils/apiInterceptors';
 
-let API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3002/api/v1';
+let API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  'https://task-managment-system-7jbd.onrender.com/api/v1';
 API_URL = API_URL.trim();
 
 const api = axios.create({
